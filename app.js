37e80fa5cebcf3246d7ec44cee3f9707b9107470
Satyam Tiwari <satyam.12207441@lpu.in>
2024-09-30 20:01:17 +0530

    Day3

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 // ====Nested ==
 import React from "react";
 import ReactDOM from "react-dom/client";
-const parent = React.createElement("div",{id: "parent"}, 
-[React.createElement("div", {id: "child1"},
-[React.createElement("h1",{},"Im an h1 tag"), 
-React.createElement("h1",{}, "Radhe Radhe")]),
-React.createElement("div",{id: "child2"},
-[React.createElement("h1",{},"Im an h1 tag"), 
-React.createElement("h1",{}, "Radhe Radhe")]
-)] 
+
+
+const heading = React.createElement("h1",{id: "heading"}, "Satyam Tiwari"); 
+
+
+const title =(
+<h1 id= "heading">Ract</h1>
 )
 
-const heading = React.createElement("h1",{id: "heading"},"Radhe Radhe");
+
+const HeadingComponent =  () =>(
+     <>
+     {title}
+     <h1>Radhe Radhe</h1>
+     </>
+)
+
+
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+
+root.render(<HeadingComponent/> );
