3e589e30d2531ef744572326a16bbb66f74fa62a
Satyam Tiwari <tiwarisatyam4685@gmail.com>
2024-10-14 23:47:22 +0530

    Lifecycle of Class base component

--- src/components/About.js
+++ src/components/About.js
@@ -1,16 +1,31 @@
 import User from "./User";
 import UserClass from "./UserClass";
+import React from "react";
 
-const About = () =>{
-    return(
-        <div>
-            <h1>About</h1>
-            <h2>Radhe Radhe</h2>
-            <User/>
 
-            <UserClass name ={"Satyam Tiwari (class)"} location ={"Punjab (class)"}/>
-        </div>
-    )
+class About extends React.Component{
+
+    constructor(props){
+        super(props);
+        console.log("parent constracter");
+    }
+
+
+    componentDidMount(){
+        console.log("parent component did mount");
+    }
+
+    render(){
+        console.log("parent render");
+        return(
+            <div>
+                <h1>About</h1>
+                <h2>Radhe Radhe</h2>
+                <UserClass name ={"Satyam Tiwari (class)"} location ={"Punjab (class)"}/>
+            </div>
+        )
+    }
 }
+
 
 export default About;
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -3,14 +3,31 @@ class UserClass extends React.Component{
 
     constructor(props){
         super(props);
-        console.log(props);
+        this.state = {
+            count: 0,
+            count2: 1,
+        }
+        console.log("child constracter");
+    
+    }
+
+    componentDidMount(){
+        console.log("child component did mount");
     }
 
 
     render(){
+        console.log("child constracter");
+        const{count} = this.state;
         const{name,location} = this.props;
         return (
             <div className="user-card">
+                <h1>Count: = {count}</h1>
+                <button onClick={() =>{
+                    this.setState({
+                        count:  this.state.count+1,
+                    })
+                }}>Increse</button>
                 <h2>Name: {name}</h2>
                 <h3>Location: {location}</h3>
                 <h3>Contact Us: tiwarisatyam@gamil.com</h3>
